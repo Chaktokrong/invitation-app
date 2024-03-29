7b2a92d69604a8b8051bf5625b009ac920bae77b
Chaktokrong <chaktokrong@gmail.com>
2024-03-29 17:39:31 +0700

    29/03/24

--- app/AutoImage/page.js
+++ app/AutoImage/page.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Stack, Image } from "@chakra-ui/react";
+// import { motion } from "framer-motion"
+// const MotionBox = motion(Box)
 
 export default function AutoImage() {
   return (
     <Stack direction="row" justifyContent="center">
-      <Image
+
+      <motion.Image
         src="../../Images/bride-and-groom.png"
         alt="header-img"
         className="invitation-img"
