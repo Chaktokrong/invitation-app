05050f7d8685c68880e8e1423ed7f1f12142bc1b
goglobalschooldev <goglobalschool15@gmail.com>
2025-04-02 07:46:01 +0700

    02/04/25

--- app/page.js
+++ app/page.js
@@ -16,12 +16,6 @@ export default function Home() {
         </Stack>
         <Text className="invitation-des">សូមគោរពអញ្ជើញ</Text>
         <Text className="invitation-des">ឯកឧត្តម និងលោកជំទាវ</Text>
-
-        <Stack direction="row" justifyContent="center">
-          <Link href="/Introduction" passHref>
-            <Button className="button-to-open">សូមចុចដើម្បីបើកសំបុត្រ</Button>
-          </Link>
-        </Stack>
       </Stack>
     </div>
   );
