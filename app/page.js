b7c827d68cc7d14425ddf3c4e64e66e97a68d53c
Lightning-9 <sarona.hrms@gmail.come.com>
2025-04-06 14:40:58 +0700

    06/04/25

--- app/page.js
+++ app/page.js
@@ -13,7 +13,8 @@ export default function Home() {
           className="top-card"
         >
           <Image
-            src="../../Images/couple-01.JPG"
+            // src="../../Images/couple-01.JPG"
+            src="../../Images/img-test.avif"
             alt="header-img"
             className="invitation-img"
           />
@@ -23,7 +24,20 @@ export default function Home() {
             className="empty-card-container"
           ></Stack>
           <Stack direction="column" spacing={4} className="top-text-container">
-            <Text className="invitation-title">ពិធីរៀបអាពាហ៍ពិពាហ៍</Text>
+            <Text className="invitation-title" mt="50px">សិរីមង្គលអាពាហ៍ពិពាហ៍</Text>
+            <Stack direction="column" spacing={1}>
+              <Text className="invitation-title">ឈ្លី​ ចតុរ៉ុង</Text>
+              <Stack direction="row" justifyContent="center">
+                <Image
+                  src="../../Images/heart-gif.webp"
+                  alt="Gif"
+                  style={{ width: "80px" }}
+                />
+              </Stack>
+              <Text className="invitation-title">ហ៊ុល សូភីម</Text>
+
+              <Text className="invitation-title">សូមគោរពអញ្ជើញ</Text>
+            </Stack>
           </Stack>
         </Stack>
         {/* <Text className="invitation-des">សូមគោរពអញ្ជើញ</Text>nn
