eecac69767fb8e0f9de56f2b54ab7b5dc40afe60
goglobalschooldev <goglobalschool15@gmail.com>
2025-04-10 08:20:24 +0700

    10/04/25

--- app/page.js
+++ app/page.js
@@ -23,11 +23,12 @@ export default function Home() {
             spacing={4}
             className="empty-card-container"
           ></Stack>
-          <Stack direction="column" spacing={4} className="top-text-container">
-            <Text className="invitation-title" mt="50px">
+          <Stack direction="column" spacing={5} className="top-text-container">
+            <Text className="invitation-title-top" mt="50px">
               សិរីមង្គលអាពាហ៍ពិពាហ៍
             </Text>
-            <Stack direction="column" spacing={0}>
+
+            <Stack direction="column" spacing={0} sx={{ paddingTop: "100px" }}>
               <Text className="people-name">ឈ្លី​ ចតុរ៉ុង</Text>
               <Stack direction="row" justifyContent="center">
                 <Image
@@ -37,34 +38,34 @@ export default function Home() {
                 />
               </Stack>
               <Text className="people-name">ហ៊ុល សូភីម</Text>
+            </Stack>
 
-              <Text className="invite-title">សូមគោរពអញ្ជើញ</Text>
+            <Stack direction="column" spacing={1} sx={{ paddingTop: "50px" }}>
+              <Text className="invitation-title">សូមគោរពអញ្ជើញ</Text>
               <img src="" />
             </Stack>
 
-            <Text className="invitation-title">
-              ថ្ងៃសៅរ ទី០៣ ខែឧសភា ឆ្នាំ២០២៥
-            </Text>
-            <Text className="invitation-des">
-              ភូមិជ្រោយនាងងួន ឃុំជ្រោយនាងងួន ស្រុកស្រីស្នំ ខេត្តសៀមរាប
-            </Text>
+            <Stack direction="column" spacing={1} sx={{ paddingTop: "50px" }}>
+              <Text className="invite-title">
+                ថ្ងៃសៅរ៍ ទី០៣ ខែឧសភា ឆ្នាំ២០២៥
+              </Text>
+              <Text className="invitation-des">
+                ភូមិជ្រោយនាងងួន ឃុំជ្រោយនាងងួន ស្រុកស្រីស្នំ ខេត្តសៀមរាប
+              </Text>
+            </Stack>
           </Stack>
         </Stack>
 
-        <Stack direction="column" spacing={2}>
-          <Text className="invitation-title">
-            យើងខ្ញុំមានកិត្តិយសសូមគោរពអញ្ជើញ
-          </Text>
-          nn
-          <Text className="invitation-des">
-            ឯកឧត្តម លោកឧកញ៉ា លោកជំទាវ លោក លោកស្រី អ្នកនាងកញ្ញា
-            អញ្ជើញចូលរួមជាអធិបតី និងជាភ្ញៀវកិត្តិយស
-            ដើម្បីប្រសិទ្ធិពរជ័យសិរីសួស្តី ជ័យមង្គល ក្នុងពិធីអាពាហ៍ពិពាហ៍
-            របស់យើងខ្ញុំទាំងពីរ។
-          </Text>
-        </Stack>
+        {/* <Stack direction="column" spacing={2}> */}
+        <Text className="invite-title">យើងខ្ញុំមានកិត្តិយសសូមគោរពអញ្ជើញ</Text>
+        <Text className="invitation-des">
+          ឯកឧត្តម លោកឧកញ៉ា លោកជំទាវ លោក លោកស្រី អ្នកនាងកញ្ញា
+          អញ្ជើញចូលរួមជាអធិបតី និងជាភ្ញៀវកិត្តិយស ដើម្បីប្រសិទ្ធិពរជ័យសិរីសួស្តី
+          ជ័យមង្គល ក្នុងពិធីអាពាហ៍ពិពាហ៍ របស់យើងខ្ញុំទាំងពីរ។
+        </Text>
+        {/* </Stack> */}
 
-        <Text className="invitation-title">ទីតាំងកម្មវិធីមង្គល</Text>
+        <Text className="invite-title">ទីតាំងកម្មវិធីមង្គល</Text>
       </Stack>
     </div>
   );
